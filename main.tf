d4edf1a1b572b8728a9029af5c3316dc6646f2a8
ugogineering <ugogineering@gmail.com>
2024-02-27 16:33:02 +0100

    Corrected errors in main.tf file by commenting them out and updating script with example from Terraform registry.

--- main.tf
+++ main.tf
@@ -15,14 +15,28 @@ resource "google_storage_bucket" "helm_repository" {
   name          = "ugo-bucket-270224"
   location      = "US"
   force_destroy = true
+
+  uniform_bucket_level_access = true 
+
+  website {
+    main_page_suffix = "index.html
+    not_found_page = "404.html"
+  }
+  cors {
+    origin = ["https://caseray.com.ng"]
+    method = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+
+
 }
 
 # Enabling website configuration on ugo-bucket 
-resource "google_storage_bucket_website" "helm_repository_website" {
-  bucket = google_storage_bucket.helm_repository.name
-
-  homepage_suffix = "index.yaml"
-}
+#resource "google_storage_bucket_website" "helm_repository_website" {
+#  bucket = google_storage_bucket.helm_repository.name
+#  homepage_suffix = "index.yaml"
+#}
 
 # Configuring CORS setting to allow Helm clients to access bucket
 resource "google_storage_bucket_iam_member" "helm_repository_cors" {
