9bbe0a687654aa233c6dedefbbbc8f556da9b23a
ugogineering <ugogineering@gmail.com>
2024-02-27 14:09:47 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+# Creating a GCS bucket to store Helm charts
+resource "google_storage_bucket" "helm_repository" {
+  name          = "ugo-bucket-270224"
+  location      = "US"
+  force_destroy = true
+}
+
+# Enabling website configuration on ugo-bucket 
+resource "google_storage_bucket_website" "helm_repository_website" {
+  bucket = google_storage_bucket.helm_repository.name
+
+  homepage_suffix = "index.yaml"
+}
+
+# Configuring CORS setting to allow Helm clients to access bucket
+resource "google_storage_bucket_iam_member" "helm_repository_cors" {
+  bucket = google_storage_bucket.helm_repository.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
