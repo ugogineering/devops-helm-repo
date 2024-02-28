62d5d59a88f94ddf93c7830c1ca94236f6379559
ugogineering <ugogineering@gmail.com>
2024-02-28 09:28:16 +0100

    Modified release.sh by removing some lines of code.

--- scripts/release.sh
+++ scripts/release.sh
@@ -18,21 +18,3 @@ helm gcs init ${GCS_BUCKET_NAME}
 # add gcs bucket as helm repo
 echo "Adding gcs bucket repo ${GCS_BUCKET_NAME}"
 helm repo add private ${GCS_BUCKET_NAME}
-
-prev_rev=$(git rev-parse HEAD^)
-echo "Identifying changed charts since git rev ${prev_rev}"
-
-changed_charts=()
-readarray -t changed_charts <<< "$(git diff --find-renames --diff-filter=d --name-only "$prev_rev" -- charts | cut -d '/' -f 2 | uniq)"
-
-if [[ -n "${changed_charts[*]}" ]]; then
-    for chart in "${changed_charts[@]}"; do
-        echo "Packaging chart '$chart'..."
-        chart_file=$(helm package "charts/$chart" | awk '{print $NF}')
-
-        echo "Pushing $chart_file..."
-        helm gcs push "$chart_file" private
-    done
-else
-    echo "No chart changes detected"
-fi
